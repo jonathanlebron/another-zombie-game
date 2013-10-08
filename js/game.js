4b66287a56d31c8d09af05bfc91ef7982f280848
Jonathan Lebron <jlebron2@gmail.com>
2013-10-07 22:04:51 -0400

    animated hero

--- js/game.js
+++ js/game.js
@@ -12,9 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+var resources = ["world", "cloud", "cloud2", "hero"];
+
 var canvas,
 	context,
-	totalResources = 1,
+	totalResources = resources.length,
 	numResourcesLoaded = 0,
 	images = {},
 	fps = 30,
@@ -25,19 +27,25 @@ var canvas,
 	cloudY = 0,
 	cloud2X = 800,
 	cloud2Y = 0,
+	cloudXSpeed = .5,
+	cloudYSpeed = 0,
 	playerX = 430,
-	playerY = 375;
+	playerY = 375,
+	playerXSpeed = 5,
+	playerYSpeed = 5,
+	playerXVel,
+	playerYVel;
 
 function setupAZG(){
-  // Create the canvas (Neccessary for IE because it doesn't know what a canvas element is)
-  canvas = document.getElementById("world");
+	// Create the canvas (Neccessary for IE because it doesn't know what a canvas element is)
+	canvas = document.getElementById("world");
 
-  context = canvas.getContext("2d"); // Grab the 2d canvas context
+	context = canvas.getContext("2d"); // Grab the 2d canvas context
 
-  loadImage("world"); //edited from http://www.spriters-resource.com/game_boy_advance/narutorpg/sheet/14388/
-  loadImage("cloud");
-  loadImage("cloud2");
-  loadImage("hero");
+	loadImage("world"); //edited from http://www.spriters-resource.com/game_boy_advance/narutorpg/sheet/14388/
+	loadImage("cloud");
+	loadImage("cloud2");
+	loadImage("hero");
 }
 
 function loadImage(name) {
@@ -45,7 +53,7 @@ function loadImage(name) {
   images[name] = new Image();
   images[name].onload = function(){
 	  resourceLoaded();
-	}
+  }
   images[name].src = "images/" + name + ".png";
 }
 
@@ -74,6 +82,28 @@ function animateClouds(){
   if (cloud2X <= -canvasWidth)
   	cloud2X = canvasWidth;
 
-  cloudX -= .5;
-  cloud2X -= .5;
+  cloudX -= cloudXSpeed;
+  cloud2X -= cloudXSpeed;
+}
+
+window.addEventListener('keydown', handleInput,false);
+function handleInput(e){
+	var key = e.keyCode;
+	playerXVel = 0.0;
+	playerYVel = 0.0;
+
+	switch (key) {
+        case 37: playerXVel -= playerXSpeed; break; //Left key
+        case 38: playerYVel -= playerYSpeed; break; //Up key
+        case 39: playerXVel += playerXSpeed; break; //Right key
+        case 40: playerYVel += playerYSpeed; break; //Down key
+        default: break; //Everything else
+    }
+
+    move();
+}
+
+function move(){
+	playerX += playerXVel;
+	playerY += playerYVel;
 }
